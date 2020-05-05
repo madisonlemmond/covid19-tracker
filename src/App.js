63d5a9b99d7f11340ea1a03500218f4537b053fb
madisonlemmond <maddielemmond@gmail.com>
2020-05-05 14:02:04 -0500

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
+import { TimeSinceOutbreak } from './components';
 
 import styles from './App.module.css';
 
 class App extends React.Component {
 
-    daysPassedCalculator = () => {
-        const originDate = new Date("12/30/2019");
-        let currentDate = new Date();
-        let differenceInTime = currentDate.getTime() - originDate.getTime();
-        let differenceInDays = differenceInTime / (1000 * 3600 * 24);
-        return Math.floor(differenceInDays);
-    }
-
     render() {
         return (
-            <div>
-                <h2>Days since COVID-19 outbreak: {this.daysPassedCalculator()}</h2>
+            <div className={styles.container}>
+                <TimeSinceOutbreak />
             </div>
         );
     }
